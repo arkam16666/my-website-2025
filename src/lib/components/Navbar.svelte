3716b1cdea4b32bc51ef2e5139d4e2b696549ff4
arkam16666 <oagam16555@gmail.com>
2025-06-14 22:49:01 +0700

    nevbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -2,9 +2,10 @@
     <div class=" bg-white/30 backdrop-blur-sm ... mt-3 p-3 pr-5 pl-5 rounded-full">
 
         <nav class=" space-x-20 text-2xl">
-            <a href="">home</a>
-            <a href="">main</a>
-            <a href="">about</a>
+            <a href="">Home</a>
+            <a href="">Project</a>
+            <a href="">Main</a>
+            <a href="">About</a>
         </nav>
     </div>
 </div>
