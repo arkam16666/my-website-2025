27359914abd47bbe7a5dae22b3b7b6a3c8cbb907
arkam16666 <oagam16555@gmail.com>
2025-06-14 19:47:16 +0700

    my-bio

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,10 @@
+<div class=" flex justify-center relative ">
+    <div class=" bg-white/30 backdrop-blur-sm ... mt-3 p-3 pr-5 pl-5 rounded-full">
+
+        <nav class=" space-x-20 text-2xl">
+            <a href="">home</a>
+            <a href="">main</a>
+            <a href="">about</a>
+        </nav>
+    </div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Nav from '$lib/components/Navbar.svelte';
+</script>
+<div class="bg-blue-500 min-h-screen z-0 absolute w-full font-kani font-bold">
+	<main>
+		<Nav />
+	</main>
+</div>
