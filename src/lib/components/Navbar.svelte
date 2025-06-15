a883ca35ef9a2d3a50866d0cf335cb46def726fc
PoTae000 <ngo81559@gmail.com>
2025-06-15 20:09:32 +0700

    edit

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -11,13 +11,12 @@
 </script>
 
 
-<div class="w-full flex justify-center  px-4 fixed top-0 left-0 right-0 z-1">
+<div class="w-full flex justify-center px-4 fixed top-0 left-0 right-0 z-1">
 	<nav
 		class=" shadow-2xl mt-4 bg-transparent backdrop-blur-sm  rounded-3xl px-6 py-4 
                w-[75%] max-w-[90%] sm:max-w-[250px] md:max-w-[600px] lg:max-w-[700px] 
                transition-all duration-300 absolute">
 
-		<!-- Mobile toggle button -->
 		<div class="flex justify-between w-full items-center sm:hidden">
 			<div class="text-xl font-bold text-gray-700">Menu</div>
 			<button aria-label="toggle" on:click={() => (isOpen = !isOpen)} class="text-gray-600 focus:outline-none">
@@ -29,23 +28,22 @@
 			</button>
 		</div>
 
-		<!-- Nav items -->
-		<ul class={`mt-4 sm:mt-0 text-center sm:text-xl lg:text-xl  text-black 
-                    ${isOpen ? '' : 'hidden'} sm:flex sm:flex-row sm:justify-center sm:space-x-6 lg:space-x-20`}>
-			<li class="py-2 sm:py-0">
-				<a href="#Home" class=" hover:text-cyan-500 transition-colors duration-300 block">{ul.Home}</a>
+		<ul class={`mt-4 sm:mt-0 font-mono text-center sm:text-xl lg:text-lg  text-black 
+                    ${isOpen ? '' : 'hidden'} sm:flex sm:flex-row sm:justify-center sm:space-x-6 lg:space-x-18 `}>
+			<li class="py-2 sm:py-0 ">
+				<a href="#Home" class="relative underline-hover hover:text-cyan-500  transition-colors duration-300 block">{ul.Home}</a>
 			</li>
 			<li class="py-2 sm:py-0">
-				<a href="#Skill" class="hover:text-cyan-500 transition-colors duration-300 block">{ul.Skill}</a>
+				<a href="#Skill" class="relative underline-hover hover:text-cyan-500 transition-colors duration-300 block">{ul.Skill}</a>
 			</li>
 			<li class="py-2 sm:py-0">
-				<a href="#Project" class="hover:text-cyan-500 transition-colors duration-300 block">{ul.Project}</a>
+				<a href="#Project" class="relative underline-hover hover:text-cyan-500 transition-colors duration-300 block">{ul.Project}</a>
 			</li>
 			<li class="py-2 sm:py-0">
-				<a href="#About" class="hover:text-cyan-500 transition-colors duration-300 block">{ul.About}</a>
+				<a href="#About" class="relative underline-hover hover:text-cyan-500 transition-colors duration-300 block">{ul.About}</a>
 			</li>
 			<li class="py-2 sm:py-0">
-				<a href="#Game" class="hover:text-cyan-500 transition-colors duration-300 block">{ul.Game}</a>
+				<a href="#Game" class="relative underline-hover hover:text-cyan-500 transition-colors duration-300 block">{ul.Game}</a>
 			</li>
 		</ul>
 	</nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,56 @@
 <script>
-	import Nav from '$lib/components/Navbar.svelte';
+	import { onMount } from 'svelte';
+
+	let text = "MY NAME IS THANADON";
+	let displayedText = '';
+	let index = 0;
+	let isDeleting = false;
+
+	onMount(() => {
+		typeWriter();
+	});
+
+	function typeWriter() {
+		const typingSpeed = isDeleting ? 60 : 120;
+		displayedText = text.slice(0, index);
+
+		if (!isDeleting) {
+			if (index < text.length) {
+				index++;
+			} else {
+				isDeleting = true;
+				setTimeout(typeWriter, 4000);
+				return;
+			}
+		} else {
+			if (index > 0) {
+				index--;
+			} else {
+				isDeleting = false;
+				setTimeout(typeWriter, 1000);
+				return;
+			}
+		}
+
+		setTimeout(typeWriter, typingSpeed);
+	}
 </script>
 
-<div class="font-kani min-h-screen bg-gradient-to-b to-cyan-100 font-bold">
-	<main class="container">
-		<p class=" text-black">Hello 👋 I'm</p>
-		<section class="animation">
-			<div class="first"><div>Carlos Córdova</div></div>
-			<div class="second"><div>Web Developer</div></div>
-			<div class="third"><div>Software Engineer</div></div>
-		</section>
+<div class="font-kanit min-h-screen bg-gradient-to-b from-white to-cyan-100/75 font-bold mt-49 flex justify-center text-center">
+	<main class="w-96">
+		<p class="text-2xl text-cyan-500 ">
+			{displayedText}<span class="border-r-1 border-cyan-500 animate-pulse ml-1"></span>
+		</p>
+			<p class="w-full mt-7 ">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti distinctio quam necessitatibus magni soluta nulla nisi eos totam, doloribus quaerat!</p>
+			<div class="flex justify-center  mt-12">
+				<div class="ring-container ">
+					<div class="rotating-ring "></div>
+					<img class="rounded-full border-4 border-white shadow-xl" width="200px" src="/favicon.png"  alt="">
+				</div>
+			</div>
+			<div class="mt-56">
+				<div class="text-7xl arrow-down "></div>
+			</div>
 	</main>
 </div>
-<div class=" h-100"></div>
+<div class="h-100"></div>
